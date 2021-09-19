e362ef02b412aa68f7de590d3553be93ca77cdbe
Fco. Javier Ordóñez Medina <ordomedina@gmail.com>
2021-09-19 14:31:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/snapshots/HomeFragment.kt
@@ -0,0 +1,108 @@
+package com.example.snapshots
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.snapshots.databinding.FragmentHomeBinding
+import com.example.snapshots.databinding.ItemSnapshotBinding
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentHomeBinding
+    private lateinit var mFirebaseAdapter: FirebaseRecyclerAdapter<Snapshot, SnapshotHolder>
+    private lateinit var mLayoutManager: RecyclerView.LayoutManager
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        mBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val query = FirebaseDatabase.getInstance().reference.child("snapshots")
+
+        val options = FirebaseRecyclerOptions.Builder<Snapshot>()
+            .setQuery(query, Snapshot::class.java).build()
+
+        mFirebaseAdapter = object : FirebaseRecyclerAdapter<Snapshot, SnapshotHolder>(options){
+            private lateinit var mContext: Context
+
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SnapshotHolder {
+                mContext = parent.context
+
+                val view = LayoutInflater.from(mContext)
+                    .inflate(R.layout.item_snapshot, parent, false)
+                return SnapshotHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: SnapshotHolder, position: Int, model: Snapshot) {
+                val snapshot = getItem(position)
+
+                with(holder){
+                    setListener(snapshot)
+
+                    binding.tvTitle.text = snapshot.title
+                    Glide.with(mContext)
+                        .load(snapshot.photoUrl)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .centerCrop()
+                        .into(binding.imgPhoto)
+                }
+            }
+
+            override fun onDataChanged() {
+                super.onDataChanged()
+                mBinding.progressBar.visibility = View.GONE
+            }
+
+            override fun onError(error: DatabaseError) {
+                super.onError(error)
+                Toast.makeText(mContext, error.message, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        mLayoutManager = LinearLayoutManager(context)
+
+        mBinding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = mLayoutManager
+            adapter = mFirebaseAdapter
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mFirebaseAdapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mFirebaseAdapter.stopListening()
+    }
+
+    inner class SnapshotHolder(view: View) : RecyclerView.ViewHolder(view){
+        val binding = ItemSnapshotBinding.bind(view)
+
+        fun setListener(snapshot: Snapshot){
+
+        }
+    }
+}
