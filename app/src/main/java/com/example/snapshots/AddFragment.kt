2d97c9ac681aa96bec4ef7edbbb4f103017ff418
Fco. Javier Ordóñez Medina <ordomedina@gmail.com>
2021-10-04 13:43:00 +0200

    Vídeo 192: hemos subido una foto a real data base, y ya podemos verla en el listado.

--- app/src/main/java/com/example/snapshots/AddFragment.kt
+++ app/src/main/java/com/example/snapshots/AddFragment.kt
@@ -67,6 +67,7 @@ class AddFragment : Fragment() {
     private fun postSnapshot() {
         mBinding.progressBar.visibility = View.VISIBLE
         //mStorageReference.child(PATH_SNAPSHOT).child("my_photo")
+        val key = mDatabaseReference.push().key!!
         val storageReference = mStorageReference.child(PATH_SNAPSHOT).child("my_photo")
         if(mPhotoSelectedUri != null) {
             storageReference.putFile(mPhotoSelectedUri!!)
@@ -80,6 +81,12 @@ class AddFragment : Fragment() {
                 }
                 .addOnSuccessListener {
                     Snackbar.make(mBinding.root, "Foto publicada", Snackbar.LENGTH_LONG).show()
+                    it.storage.downloadUrl.addOnSuccessListener {
+                        saveSnapshot(key, it.toString(), mBinding.etTitle.text.toString().trim())
+                        mBinding.tilTitle.visibility = View.GONE
+                        mBinding.tvMessage.text = getString(R.string.post_message_title)
+                    }
+
                 }
                 .addOnFailureListener {
                     Snackbar.make(mBinding.root, "No se pudo subir foto", Snackbar.LENGTH_LONG).show()
@@ -87,8 +94,9 @@ class AddFragment : Fragment() {
         }
     }
 
-    private fun saveSnapshot(){
-
+    private fun saveSnapshot(key: String, url: String, title: String){
+        val snapshot = Snapshot(title = title, photoUrl = url)
+        mDatabaseReference.child(key).setValue(snapshot)
     }
 
  /*   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
