2c4e5c4c2aca10792e1051b64b745fc8e8a54109
Fco. Javier Ordóñez Medina <ordomedina@gmail.com>
2021-10-06 17:45:16 +0200

    Vídeo 197: ya estamos subiendo las imágenes con nombres dinámicos para que no se solapen en la storage.

--- app/src/main/java/com/example/snapshots/AddFragment.kt
+++ app/src/main/java/com/example/snapshots/AddFragment.kt
@@ -12,6 +12,7 @@ import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.snapshots.databinding.FragmentAddBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
@@ -68,7 +69,8 @@ class AddFragment : Fragment() {
         mBinding.progressBar.visibility = View.VISIBLE
         //mStorageReference.child(PATH_SNAPSHOT).child("my_photo")
         val key = mDatabaseReference.push().key!!
-        val storageReference = mStorageReference.child(PATH_SNAPSHOT).child("my_photo")
+        val storageReference = mStorageReference.child(PATH_SNAPSHOT)
+            .child(FirebaseAuth.getInstance().currentUser!!.uid).child(key)
         if(mPhotoSelectedUri != null) {
             storageReference.putFile(mPhotoSelectedUri!!)
                 .addOnProgressListener {
